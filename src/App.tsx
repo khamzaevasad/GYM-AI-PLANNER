b28fda2538ee7fc6477ee98b5872951dca0f64d0
khamzaevasad <xamzayevasad442@gmail.com>
2026-03-08 20:51:47 +0900

    add Navbar and Button components; update Home page and package dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,23 @@ import Onboarding from "./pages/Onboarding";
 import Profile from "./pages/Profile";
 import Auth from "./pages/Auth";
 import Account from "./pages/Account";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/onboarding" element={<Onboarding />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/auth/:pathname" element={<Auth />} />
-        <Route path="/account/:pathname" element={<Account />} />
-      </Routes>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/onboarding" element={<Onboarding />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/auth/:pathname" element={<Auth />} />
+            <Route path="/account/:pathname" element={<Account />} />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
